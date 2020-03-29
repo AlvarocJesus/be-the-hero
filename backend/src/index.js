f7b36c3cc6b1b31fccca6dac820ad8adf90f0fb8
alvaro <alvim.coelho.jesus@gmail.com>
2020-03-29 16:16:57 -0300

    Mobile da aplicacao

---
+++ backend/src/index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
+
+/**
+ * Rota / Recursos
+ https://github.com/AlvarocJesus/be-the-hero.git
+*/
+
+/**
+ * Metodos HTTP:
+ * GET: Buscar informacao do back-end
+ * POST: Criar uma informacao  no back-end
+ * PUT: Alteral informacao no back-end
+ * Delete: Deletar uma informacao no back-end
+*/
+
+/**
+ * Tipos de parametros:
+ * 
+ * Query Params: Parametros nomeados enviados na rota apos o simbolo de "?" (filtros, paginacao)
+ * Rout Params: Parametros para identificar recursos
+ * Request Body: Corpo da requisicao, utilizado para criar ou alterar recursos
+*/
+
+/**
+  * SQL: MySQL, SQLite, PortgreSQL, Oracle, Microsoft SQL Server
+  * NoSQL: MongoDB, CouchDB, etc
+*/
+
+/**
+ * Driver: Select*from users
+ * query vuilder: table('users').select('*').where()
+*/
+
+
+
